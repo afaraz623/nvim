8e775dbe1def6329d60f7dd0e7f44bebbac6f3dd
Faraz Ahmed Khan <afaraz623@gmail.com>
2024-03-30 12:15:52 +0500

    Added some helpful fugitive remaps

--- lua/farrow/plugins/git.lua
+++ lua/farrow/plugins/git.lua
@@ -1,6 +1,13 @@
 return {
   {
-    "tpope/vim-fugitive"
+    "tpope/vim-fugitive",
+    config = function()
+      vim.keymap.set("n", "<Leader>gt", "<cmd>Git<cr>", {})
+      vim.keymap.set("n", "<Leader>gl", "<cmd>Git log<cr>", {})
+      vim.keymap.set("n", "<Leader>ga", "<cmd>Git add .<cr>", {})
+      vim.keymap.set("n", "<Leader>gs", "<cmd>Git status<cr>", {})
+      vim.keymap.set("n", "<Leader>gp", "<cmd>Git push<cr>", {})
+    end,
   },
   {
     "lewis6991/gitsigns.nvim",
@@ -10,6 +17,6 @@ return {
 
       --set keymap
       vim.keymap.set("n", "<Leader>gp", ":Gitsigns preview_hunk<CR>", {})
-    end
-  }
+    end,
+  },
 }
