8ec755a090cc216604354bf7f143887353cc70bb
Faraz Ahmed Khan <afaraz623@gmail.com>
2024-03-30 11:54:06 +0500

    Added a few fugitive keymaps

--- lua/farrow/plugins/git.lua
+++ lua/farrow/plugins/git.lua
@@ -1,6 +1,14 @@
 return {
   {
-    "tpope/vim-fugitive"
+    "tpope/vim-fugitive",
+
+    config = function()
+      -- set keymap
+      vim.keymap.set("n", "<Leader>gt", "<cmd>Git<cr>", {})
+      vim.keymap.set("n", "<Leader>ga", "<cmd>Git add .<cr>", {})
+      vim.keymap.set("n", "<Leader>gs", "<cmd>Git status<cr>", {})
+      vim.keymap.set("n", "<Leader>gp", "<cmd>Git push<cr>", {})
+    end,
   },
   {
     "lewis6991/gitsigns.nvim",
@@ -9,7 +17,7 @@ return {
       require("gitsigns").setup()
 
       --set keymap
-      vim.keymap.set("n", "<Leader>gp", ":Gitsigns preview_hunk<CR>", {})
-    end
-  }
+      vim.keymap.set("n", "<Leader>gh", ":Gitsigns preview_hunk<CR>", {})
+    end,
+  },
 }
