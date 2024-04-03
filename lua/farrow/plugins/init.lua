3f42bdfd17c2efefeea2a9126baa4901733e0634
Faraz Ahmed Khan <afaraz623@gmail.com>
2024-04-03 05:30:38 +0500

    Removed small setup plugins into plugins/init.lua

--- lua/farrow/plugins/init.lua
+++ lua/farrow/plugins/init.lua
@@ -1,7 +1,53 @@
-return
-{
+return {
   -- Plugins with no complex configurations are installed here otherwise, look in a specific plugin folder.
 
-  -- tmux navigation
-  "christoomey/vim-tmux-navigator",
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000,
+    config = function()
+      -- globally set theme
+      vim.cmd.colorscheme("rose-pine")
+
+      -- set number line colour to white
+      vim.cmd([[highlight LineNr ctermfg=white guifg=white]])
+    end,
+  },
+  {
+    -- tmux navigation
+    "christoomey/vim-tmux-navigator",
+  },
+  {
+    -- plugin for auto completeing brackets quotations etc etc
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+  },
+  {
+    -- adds comments functionality
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+
+    config = function()
+      -- import comment plugin safely
+      local comment = require("Comment")
+
+      -- enabling comment and changing default binding to ?
+      comment.setup({
+        opleader = { line = "?" },
+      })
+    end,
+  },
+  {
+    -- this allows me to have many many undos
+    "mbbill/undotree",
+
+    -- set keymaps
+    vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<cr>", { desc = "Toggle UndoTree" }),
+  },
+  {
+    -- a pretty input box maker
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
+  },
 }
--- lua/farrow/plugins/undo-tree.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'mbbill/undotree',
-
-  -- set keymaps
-  vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<cr>", { desc = "Toggle UndoTree" })
-}
