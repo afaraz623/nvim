406ddb599395817d325568ebf2b2a6f87120ebd2
Faraz Ahmed Khan <afaraz623@gmail.com>
2024-04-04 09:10:37 +0500

    made pyright work and disabled lua lsp error

--- lua/farrow/plugins/lsp.lua
+++ lua/farrow/plugins/lsp.lua
@@ -85,10 +85,7 @@ return {
           },
         },
 
-        pyright = {
-          capabilities = capabilities,
-          cmd = {},
-        },
+        pyright = {},
 
         rust_analyzer = {},
 
@@ -99,7 +96,7 @@ return {
                 callSnippet = "Replace",
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = { disable = { "missing-fields" } },
             },
           },
         },
